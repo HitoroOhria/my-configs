57f7a5bb9ca4488b84fb0bcd1fff8636d9ccc065
hiroto.ohira <hiroto100114@gmail.com>
2021-11-30 20:21:50 +0900

    Add key repeat config of mac os.

--- mac_os/setup.sh
+++ mac_os/setup.sh
@@ -3,3 +3,6 @@
 # set key speed
 defaults write -g InitialKeyRepeat -int 10
 defaults write -g KeyRepeat -int 1
+
+# set able to key repeat on any software
+defaults write -g ApplePressAndHoldEnabled -bool false
