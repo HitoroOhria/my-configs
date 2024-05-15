c4a0989dbf6de77ff5b88adaef9f3beff38e9a0c
hiroto.ohira <hiroto100114@gmail.com>
2024-05-15 14:57:57 +0900

    Add logicol g304 key config.

--- keyd/setup.sh
+++ keyd/setup.sh
@@ -3,4 +3,5 @@
 sudo ln -s $(pwd)/common.conf /etc/keyd/
 sudo ln -s $(pwd)/corne-cherry.conf /etc/keyd/
 sudo ln -s $(pwd)/laptop-keyboard.conf /etc/keyd/
+sudo ln -s $(pwd)/logicool-g304.conf /etc/keyd/
 
