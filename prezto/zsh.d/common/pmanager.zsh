f3bb411d992b5d193e0397b07b0e15cde0b3ae94
hiroto.ohira <hiroto100114@gmail.com>
2024-08-30 15:12:19 +0900

    ✨ feat: Add 'mise ls' aliases for common languages in pmanager.zsh

--- prezto/zsh.d/common/pmanager.zsh
+++ prezto/zsh.d/common/pmanager.zsh
@@ -3,4 +3,9 @@ export PATH=$PATH:/opt/homebrew/bin
 
 # set mise
 eval "$(mise activate zsh)"
+alias ml="mise ls"
+alias mlg="mise ls | grep --color=never go"
+alias mln="mise ls | grep --color=never node"
+alias mlp="mise ls | grep --color=never python"
+alias mlr="mise ls | grep --color=never rust"
 
