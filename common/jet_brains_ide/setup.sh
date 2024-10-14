4ccc1e272f51e3aef5528f94f54050f5ec02e8d7
hiroto.ohira <hiroto100114@gmail.com>
2024-10-14 20:27:39 +0900

    🚑 fix: update DataGrip console database IDs in setup script

--- common/jet_brains_ide/setup.sh
+++ common/jet_brains_ide/setup.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 # InelliJ IDEA scratches
-ln -sf $(pwd)/IntelliJIdea/scratches  ~/.config/JetBrains/IntelliJIdea2024.2
+ln -sf $(pwd)/IntelliJIdea/scratches ~/.config/JetBrains/IntelliJIdea2024.2
 
 # DataGrip consoles
+# - database id is different per machine
+# - dchange id to actual one
 ## IdeaCenter
-# ln -sf $(pwd)/DataGrid/consoles/circus-local ~/.config/JetBrains/DataGrip2024.2/consoles/db/6a3c934d-c510-456f-9c48-6f83daad9a0f
+# ln -sf $(pwd)/DataGrid/consoles/circus-local ~/.config/JetBrains/DataGrip2024.2/consoles/db/87de4bcb-4a16-4ac8-96e7-b38da704da97
+# ln -sf $(pwd)/DataGrid/consoles/circus-dev1  ~/.config/JetBrains/DataGrip2024.2/consoles/db/97fc6725-837e-431e-a574-dfc1a5a59834
+# ln -sf $(pwd)/DataGrid/consoles/circus-dev   ~/.config/JetBrains/DataGrip2024.2/consoles/db//a4445c10-148c-42c2-b9b7-28c61a376cdb
 ## LIFEBOOK WU2
 # ln -sf $(pwd)/DataGrid/consoles/circus-local ~/.config/JetBrains/DataGrip2024.2/consoles/db/6a3c934d-c510-456f-9c48-6f83daad9a0f
 # ln -sf $(pwd)/DataGrid/consoles/circus-dev1  ~/.config/JetBrains/DataGrip2024.2/consoles/db/aa5ac5fc-67df-4c52-add2-ef1ffb006aeb
