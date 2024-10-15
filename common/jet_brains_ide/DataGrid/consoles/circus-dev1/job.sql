cb11262162466924fc9d64d06b01011dee17b73a
hiroto.ohira <hiroto100114@gmail.com>
2024-10-15 19:46:20 +0900

    ✨ feat: Add job SQL script for fetching and updating occupations in DataGrid circus-dev1.

---
+++ common/jet_brains_ide/DataGrid/consoles/circus-dev1/job.sql
@@ -0,0 +1,28 @@
+SELECT id, occupations_sub FROM jobs WHERE JSON_LENGTH(occupations_sub) = 0;
+
+# Occupation
+-- occupations_main: 23
+-- occupations_sub: [46]
+SELECT occupation_main, occupations_sub FROM jobs WHERE id = 99268;
+
+-- join to masters
+SELECT j.id AS job_id, m1.name AS occupation_main, m2.name AS occupations_sub
+FROM jobs j
+LEFT JOIN masters m1
+    ON m1.value = j.occupation_main
+    AND m1.group_name = 'minor-occupations'
+    AND m1.parent_id IS NOT NULL
+LEFT JOIN masters m2
+    ON JSON_CONTAINS(j.occupations_sub, CAST(m2.value AS json))
+    AND m2.group_name = 'minor-occupations'
+    AND m2.parent_id IS NOT NULL
+WHERE j.id = 99268
+;
+
+UPDATE jobs
+SET
+#     occupation_main = 23,
+    occupations_sub = '[46]'
+#     occupations_sub = '[]'
+WHERE id = 99268
+;
